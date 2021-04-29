87543aa9ebbb9073b15ba49c10f1adb31826900b
noaccessl <54954576+noaccessl@users.noreply.github.com>
2021-04-29 20:44:35 +0300

    optimization and some changes

--- dynamic_channels.lua
+++ dynamic_channels.lua
@@ -1,60 +1,58 @@
--- With this script, you can have a dynamic temp channel for each user
--- The principle of operation is similar to that of the DynChanZL bot
+-- This script allows create a dynamic temporary voice channel for each user which can configure him as he wants
+-- Works the same as the DynChanZL bot, but on Lua
 
 local discordia = require 'discordia'
 local client = discordia.Client()
 
 local Channels = {
-    [ 'ChannelID' ] = { -- id of the channel, which will be create a dynamic temporary channel for certain user
+    [ 'ChannelID' ] = { -- Channel ID through which a dynamic temporary channel will be created for a specific user
         GuildID = 'GuildID',
-        CategoryID = 'CategoryID' -- id of category wherein will be create dynamic temporary channel
+        CategoryID = 'CategoryID' -- Category ID wherein will be create dynamic temporary channel
     }
 }
-local MoveDown = 0
 
+local slotpos = 0
 local DynamicChannels = {}
-local DynamicChannelsAlt = {}
 
 client:on( 'voiceChannelJoin', function( member, channel )
-    local ch = Channels[ channel.id ]
-    if ch ~= nil then
-        local DynamicChannel = client:getGuild( ch.GuildID ):createVoiceChannel( '👉 ' .. member.username )
-        DynamicChannel:setCategory( ch.CategoryID )
-	    DynamicChannel:moveDown( MoveDown + #DynamicChannels )
+    local ChannelData = Channels[ channel.id ]
+    if ChannelData ~= nil then
+        local category = client:getGuild( ChannelData.GuildID ):getChannel( ChannelData.CategoryID )
+
+        local DynamicChannel = category:createVoiceChannel( '👉 ' .. member.username )
+	    DynamicChannel:moveDown( slotpos + #DynamicChannels )
 
 	    member:setVoiceChannel( DynamicChannel.id )
 
 	    local permissions = DynamicChannel:getPermissionOverwriteFor( member )
 	    permissions:allowAllPermissions()
 
-        DynamicChannelsAlt[ DynamicChannel.id ] = true
-        DynamicChannels[ DynamicChannel.id ] = { members = 1, guildid = ch.GuildID }
+        local cachedData = { members = 1, guildid = ChannelData.GuildID }
+        DynamicChannels[ DynamicChannel.id ] = cachedData
     
         return
     end
 
-    if DynamicChannelsAlt[ channel.id ] then
+    if DynamicChannels[ channel.id ] ~= nil then
         DynamicChannels[ channel.id ].members = DynamicChannels[ channel.id ].members + 1
     end
 end )
 
 client:on( 'voiceChannelLeave', function( member, channel )
-    if DynamicChannelsAlt[ channel.id ] then
+    if DynamicChannels[ channel.id ] ~= nil then
         DynamicChannels[ channel.id ].members = DynamicChannels[ channel.id ].members - 1
 
         if DynamicChannels[ channel.id ].members == 0 then
             local DynamicChannel = client:getGuild( DynamicChannels[ channel.id ].guildid ):getChannel( channel.id )
             DynamicChannel:delete()
 
-            DynamicChannelsAlt[ channel.id ] = nil
             DynamicChannels[ channel.id ] = nil
         end
     end
 end )
 
 client:on( 'channelDelete', function( channel )
-    if DynamicChannelsAlt[ channel.id ] then
-        DynamicChannelsAlt[ channel.id ] = nil
+    if DynamicChannels[ channel.id ] ~= nil then
         DynamicChannels[ channel.id ] = nil
     end
 end )
