f079b5d5c1cbfc0ef08f97d940072e0edd5a267b
noaccessl <54954576+noaccessl@users.noreply.github.com>
2021-07-27 00:20:56 +0300

    Reworked permissions setup, more stability

--- dynamic_channels.lua
+++ dynamic_channels.lua
@@ -1,60 +1,194 @@
 -- This script allows create a dynamic temporary voice channel for each user which can configure him as he wants
 -- Works the same as the DynChanZL bot, but on Lua
 
-local discordia = require 'discordia'
+local discordia = require( 'discordia' )
 local client = discordia.Client()
 
-local Channels = {
-    [ 'ChannelID' ] = { -- Channel ID through which a dynamic temporary channel will be created for a specific user
-        GuildID = 'GuildID',
-        CategoryID = 'CategoryID' -- Category ID wherein will be create dynamic temporary channel
-    }
+--[[---------------------------------------------------------------------------
+Dynamic Channels
+---------------------------------------------------------------------------]]
+dynchan = {
+	channels = {
+		[ 'ChannelID' ] = { -- After connecting to the channel, which id is written here, the bot will create its own dynamic channel for you
+			GuildID = 'GuildID', -- Guild ID of this channel ( Required! )
+			CategoryID = 'CategoryID', -- Category ID of this channel ( Required! )
+			AllowedPermissions = { -- Allows the owner of a dynamic channel to:
+				0x0000000010, -- Manage it
+				0x0010000000, -- Manage its access for other members or roles
+				0x0000400000, -- Mute members of his channel
+				0x0000800000, -- Deafen members of his channel
+				0x0001000000 -- Move or disconnect members of his channel
+			} -- Read more about this here: https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+		}
+	},
+	slotpos = 0,
+	stored = {},
+	memberchannels = {}
 }
 
-local slotpos = 0
-local DynamicChannels = {}
+function dynchan.GetStored( channelID )
+
+	return dynchan.stored[ channelID ]
+
+end
+
+function dynchan.GetCount()
+
+	local count = 0
+
+	for k in pairs( dynchan.stored ) do
+
+		count = count + 1
+
+	end
+
+	return count
+
+end
+
+function dynchan.IsStored( channelID )
+
+	return dynchan.GetStored( channelID ) ~= nil
+
+end
+
+function dynchan.Store( channelID, guildID )
+
+	dynchan.stored[ channelID ] = {
+		memberCount = 1,
+		guildID = guildID
+	}
+
+end
+
+function dynchan.Remove( channelID )
+
+	dynchan.stored[ channelID ] = nil
+
+end
+
+function dynchan.MemberJoin( channelID )
+
+	local stored = dynchan.GetStored( channelID )
+	if not stored then return end
+
+	stored.memberCount = stored.memberCount + 1
+
+end
+
+function dynchan.MemberLeave( channelID )
+
+	local stored = dynchan.GetStored( channelID )
+	if not stored then return end
+
+	stored.memberCount = stored.memberCount - 1
+
+end
+
+function dynchan.GetMemberChannel( member )
+
+	return dynchan.memberchannels[ member.user.id ]
+
+end
+
+function dynchan.SetMemberChannel( member, channelID )
+
+	dynchan.memberchannels[ member.user.id ] = channelID
+
+end
+
+local timer = require( 'timer' )
 
 client:on( 'voiceChannelJoin', function( member, channel )
-    local ChannelData = Channels[ channel.id ]
-    if ChannelData ~= nil then
-        local category = client:getGuild( ChannelData.GuildID ):getChannel( ChannelData.CategoryID )
 
-        local DynamicChannel = category:createVoiceChannel( '👉 ' .. member.username )
-	    DynamicChannel:moveDown( slotpos + #DynamicChannels )
+	local channelData = dynchan.channels[ channel.id ]
 
-	    member:setVoiceChannel( DynamicChannel.id )
+	if channelData then
 
-	    local permissions = DynamicChannel:getPermissionOverwriteFor( member )
-	    permissions:allowAllPermissions()
+		local co = coroutine.create( function()
 
-        local cachedData = { members = 1, guildid = ChannelData.GuildID }
-        DynamicChannels[ DynamicChannel.id ] = cachedData
-    
-        return
-    end
+			local memberchannel = dynchan.GetMemberChannel( member )
 
-    if DynamicChannels[ channel.id ] ~= nil then
-        DynamicChannels[ channel.id ].members = DynamicChannels[ channel.id ].members + 1
-    end
+			if memberchannel then
+
+				local stored = dynchan.GetStored( memberchannel )
+
+				if stored then
+
+					dynchan.SetMemberChannel( member, nil )
+
+					local storedMemberChannel = client:getGuild( stored.guildID ):getChannel( memberchannel )
+
+					if storedMemberChannel then
+
+						storedMemberChannel:delete()
+
+					end
+
+				end
+
+			end
+
+			local category = client:getGuild( channelData.GuildID ):getChannel( channelData.CategoryID )
+
+			local DynamicChannel = category:createVoiceChannel( '👉 ' .. member.username )
+			DynamicChannel:moveDown( dynchan.slotpos + dynchan.GetCount() )
+
+			local permissions = DynamicChannel:getPermissionOverwriteFor( member )
+			permissions:allowPermissions( unpack( channelData.AllowedPermissions ) )
+
+			if member.voiceChannel == nil then
+
+				return DynamicChannel:delete()
+
+			end
+
+			member:setVoiceChannel( DynamicChannel.id )
+
+			dynchan.Store( DynamicChannel.id, channelData.GuildID )
+			dynchan.SetMemberChannel( member, DynamicChannel.id )
+
+		end )
+		timer.setTimeout( 150, coroutine.resume, co )
+
+	elseif dynchan.IsStored( channel.id ) then
+
+		dynchan.MemberJoin( channel.id )
+
+	end
+
 end )
 
 client:on( 'voiceChannelLeave', function( member, channel )
-    if DynamicChannels[ channel.id ] ~= nil then
-        DynamicChannels[ channel.id ].members = DynamicChannels[ channel.id ].members - 1
 
-        if DynamicChannels[ channel.id ].members == 0 then
-            local DynamicChannel = client:getGuild( DynamicChannels[ channel.id ].guildid ):getChannel( channel.id )
-            DynamicChannel:delete()
+	if dynchan.IsStored( channel.id ) then
 
-            DynamicChannels[ channel.id ] = nil
-        end
-    end
+		dynchan.MemberLeave( channel.id )
+
+		local stored = dynchan.GetStored( channel.id )
+
+		if stored.memberCount == 0 then
+
+			dynchan.SetMemberChannel( member, nil )
+			client:getGuild( stored.guildID ):getChannel( channel.id ):delete()
+
+		end
+
+	end
+
 end )
 
 client:on( 'channelDelete', function( channel )
-    if DynamicChannels[ channel.id ] ~= nil then
-        DynamicChannels[ channel.id ] = nil
-    end
+
+	if dynchan.IsStored( channel.id ) then
+
+		dynchan.Remove( channel.id )
+
+	end
+
 end )
 
+--[[---------------------------------------------------------------------------
+Run bot
+---------------------------------------------------------------------------]]
 client:run 'Bot BOT_TOKEN'
